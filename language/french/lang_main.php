1f37f4bda4b5d8be3f9a6653bcd0140996ef7aa3
corgan <corgan@eqdkp-plus.eu>
2010-07-09 15:58:20 +0000

    french & spanish translation

--- language/french/lang_main.php
+++ language/french/lang_main.php
@@ -1,80 +1,55 @@
 <?php
-/*
- * Project:     EQdkp Shoutbox
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:        http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date$
- * -----------------------------------------------------------------------
- * @author      $Author$
- * @copyright   2008 Aderyn
- * @link        http://eqdkp-plus.com
- * @package     shoutbox
- * @version     $Rev$
- *
- * $Id$
- */
 
-if (!defined('EQDKP_INC'))
-{
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$lang = array(
-  'shoutbox'                        => 'Shoutbox',
-  'sb_shoutbox'                     => 'Shoutbox',
+//Language: French	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 13:55
+//File: plugin_shoutbox
+//Source-Language: english
 
-  // Description
-  'sb_short_desc'                   => 'Shoutbox',
-  'sb_long_desc'                    => 'Shoutbox is a Plugin users are able to exchange short messages with.',
-
-  // General
-  'sb_plugin_not_installed'         => 'Shoutbox Plugin not installed',
-  'sb_php_version'                  => "Shoutbox requires PHP %1\$s or higher. Your server runs PHP %2\$s",
-  'sb_plus_version'                 => "Shoutbox requires EQDKP-PLUS %1\$s or higher. Your installed Version is %2\$s",
-
-  // Menu
-  'sb_manage'                       => 'Manage',
-
-  // Admin -> Settings
-  'sb_date_format'                  => 'Y/m/d H:i',  // YYYY/MM/DD HH:mm
-  'sb_time_format'                  => 'H:i',        // HH:mm
-  'sb_adm_date'                     => 'Date',
-  'sb_adm_name'                     => 'Name',
-  'sb_adm_text'                     => 'Message',
-  'sb_adm_select_all'               => 'Select all',
-  'sb_adm_select_none'              => 'Select none',
-
-  // Configuration
-  'sb_config_saved'                 => 'Settings saved successfully',
-  'sb_header_general'               => 'General Shoutbox settings',
-  'sb_updatecheck'                  => 'Enable check for new Plugin Versions',
-  'sb_timezone'                     => 'Timezone of the server',
-  'sb_dstcorrect'                   => 'Auto correct Daylight saving time?',
-
-  // Portal Modules
-  'sb_output_count_limit'           => 'Limit of shoutbox entries.',
-  'sb_show_date'                    => 'Show date also?',
-  'sb_show_archive'                 => 'Show Archive?',
-  'sb_input_box_below'              => 'Input box below entries?',
-  'sb_autoreload'                   => 'Time in seconds to wait for automatic reload of Shoutbox (Default 0 = Off)',
-  'sb_autoreload_help'              => 'Set to 0 to disable automatic reload',
-  'sb_invisible_to_guests'          => '<u>Not</u> visible to guests?',
-  'sb_no_character_assigned'        => 'No characters are connected yet. At least one character has to be connected to be able to post.',
-  'sb_submit_text'                  => 'Send',
-  'sb_save_wait'                    => 'Saving, please wait...',
-  'sb_reload'                       => 'Reload',
-  'sb_no_entries'                   => 'No entries',
-  'sb_archive'                      => 'Archive',
-  'sb_shoutbox_archive'             => 'Shoutbox Archive',
-  'sb_footer'                       => "... %1\$d found / %2\$d per page",
-
-  // About/Credits
-  'sb_about_header'                 => 'About Shoutbox',
-  'sb_credits_part1'                => 'Shoutbox v',
-  'sb_credits_part2'                => ' by Aderyn',
-  'sb_copyright'                    => 'Copyright',
-);
-
+$alang = array( 
+"shoutbox" => "Shoutbox",
+"sb_shoutbox" => "Shoutbox",
+"sb_short_desc" => "Shoutbox",
+"sb_long_desc" => "La Shoutbox est un plugin de dialogue à travers des messages courts entre les membres",
+"sb_plugin_not_installed" => "Le plugin Shoutbox n'est pas installé",
+"sb_php_version" => "Shoutbox nécessite une version PHP %1$s ou supérieure. Votre serveur utilise la version %2$s",
+"sb_plus_version" => "Shoutbox nécessite une version EQDKP-Plus %1$s ou supérieure. Votre version est la %2$s",
+"sb_manage" => "Gérer",
+"sb_date_format" => "d/m/Y H:i",
+"sb_time_format" => "H:i",
+"sb_adm_date" => "Date",
+"sb_adm_name" => "Nom",
+"sb_adm_text" => "Message",
+"sb_adm_select_all" => "Tout sélectionner",
+"sb_adm_select_none" => "Déselectionner",
+"sb_config_saved" => "Les paramètres ont été enregistrés avec succès",
+"sb_header_general" => "Paramètres généraux de Shoutbox",
+"sb_updatecheck" => "Activer la vérification de nouvelle version",
+"sb_timezone" => "Heure du serveur",
+"sb_dstcorrect" => "Auto correct Daylight saving time?",
+"sb_output_count_limit" => "Limit of shoutbox entries.",
+"sb_show_date" => "Show date also?",
+"sb_show_archive" => "Show Archive?",
+"sb_input_box_below" => "Input box below entries?",
+"sb_autoreload" => "Time in seconds to wait for automatic reload of Shoutbox (Default 0 = Off)",
+"sb_autoreload_help" => "Set to 0 to disable automatic reload",
+"sb_invisible_to_guests" => "<u>Not</u> visible to guests?",
+"sb_no_character_assigned" => "No characters are connected yet. At least one character has to be connected to be able to post.",
+"sb_submit_text" => "Send",
+"sb_save_wait" => "Saving, please wait...",
+"sb_reload" => "Reload",
+"sb_no_entries" => "No entries",
+"sb_archive" => "Archive",
+"sb_shoutbox_archive" => "Shoutbox Archive",
+"sb_footer" => "... %1$d found / %2$d per page",
+"sb_about_header" => "About Shoutbox",
+"sb_credits_part1" => "Shoutbox v",
+"sb_credits_part2" => " by Aderyn",
+"sb_copyright" => "Copyright",
+ );
+$lang = (is_array($lang))? $lang : array();
+$lang = array_merge($lang, $alang);
 ?>
--- language/spanish/lang_main.php
+++ language/spanish/lang_main.php
@@ -1,80 +1,55 @@
 <?php
-/*
- * Project:     EQdkp Shoutbox
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:        http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2008
- * Date:        $Date$
- * -----------------------------------------------------------------------
- * @author      $Author$
- * @copyright   2008 Aderyn
- * @link        http://eqdkp-plus.com
- * @package     shoutbox
- * @version     $Rev$
- *
- * $Id$
- */
 
-if (!defined('EQDKP_INC'))
-{
-    header('HTTP/1.0 404 Not Found');exit;
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$lang = array(
-  'shoutbox'                        => 'Shoutbox',
-  'sb_shoutbox'                     => 'Shoutbox',
+//Language: Spanish	
+//Created by EQdkp Plus Translation Tool on  2010-07-09 15:10
+//File: plugin_shoutbox
+//Source-Language: english
 
-  // Description
-  'sb_short_desc'                   => 'Shoutbox',
-  'sb_long_desc'                    => 'Shoutbox es un plugin que permite intercambiar mensajes cortos entre usuarios.',
-
-  // General
-  'sb_plugin_not_installed'         => 'El plugin Shoutbox no estÃ¡ instalado',
-  'sb_php_version'                  => "Shoutbox requiere la versiÃ³n PHP %1\$s o superior. TÃº servidor esta ejecutandose en PHP %2\$s",
-  'sb_plus_version'                 => "Shoutbox requiere la versiÃ³n de EQDKP-PLUS %1\$s o superior. Tienes instalado la versiÃ³n %2\$s",
-
-  // Menu
-  'sb_manage'                       => 'Administrar',
-
-  // Admin -> Settings
-  'sb_date_format'                  => 'Y/m/d H:i',  // YYYY/MM/DD HH:mm
-  'sb_time_format'                  => 'H:i',        // HH:mm
-  'sb_adm_date'                     => 'Fecha',
-  'sb_adm_name'                     => 'Nombre',
-  'sb_adm_text'                     => 'Mensaje',
-  'sb_adm_select_all'               => 'Seleccionar todos',
-  'sb_adm_select_none'              => 'No seleccionar nada',
-
-  // Configuration
-  'sb_config_saved'                 => 'Los ajustes se han almacenado correctamente',
-  'sb_header_general'               => 'Ajustes generales del Shoutbox',
-  'sb_updatecheck'                  => 'Habilitar casilla de verificaciÃ³n para las nuevas versiones Plugin',
-  'sb_timezone'                     => 'Zona horaria del servidor',
-  'sb_dstcorrect'                   => 'Â¿Ajustar automÃ¡ticamente al horario de verano?',
-
-  // Portal Modules
-  'sb_output_count_limit'           => 'Limitar entradas del shoutbox.',
-  'sb_show_date'                    => 'Â¿Mostrar las fechas?',
-  'sb_show_archive'                 => 'Mostrar Archivo?',
-  'sb_input_box_below'              => 'Â¿Caja de texto debajo de las entradas?',
-  'sb_autoreload'                   => 'Cantidad de segundos a esperar para que recargue automaticamente el Shoutbox (Por defecto 0 = Off)',
-  'sb_autoreload_help'              => 'Seleccionar 0 para desactivar la recarga automÃ¡tica',
-  'sb_invisible_to_guests'          => '<u>Invisible</u> para los invitados?',
-  'sb_no_character_assigned'        => 'AÃºn no hay usuarios conectados. Al menos un usuario tiene que estar conectado para poder enviar una entrada.',
-  'sb_submit_text'                  => 'EnvÃ­ar',
-  'sb_save_wait'                    => 'Almacenando, por favor espere...',
-  'sb_reload'                       => 'Recargar',
-  'sb_no_entries'                   => 'No hay entradas',
-  'sb_archive'                      => 'Archivo',
-  'sb_shoutbox_archive'             => 'Archivo Shoutbox',
-  'sb_footer'                       => "... %1\$d encontrado / %2\$d por pÃ¡ginas",
-
-  // About/Credits
-  'sb_about_header'                 => 'Acerca de Shoutbox',
-  'sb_credits_part1'                => 'Shoutbox v',
-  'sb_credits_part2'                => ' por Aderyn',
-  'sb_copyright'                    => 'Copyright',
-);
-
+$alang = array( 
+"shoutbox" => "Shoutbox",
+"sb_shoutbox" => "Shoutbox",
+"sb_short_desc" => "Shoutbox",
+"sb_long_desc" => "Shoutbox es un plugin que permite intercambiar mensajes cortos entre usuarios.",
+"sb_plugin_not_installed" => "El plugin Shoutbox no está instalado",
+"sb_php_version" => "Shoutbox requiere la versión PHP %1$s o superior. Tú servidor esta ejecutandose en PHP %2$s",
+"sb_plus_version" => "Shoutbox requiere la versión de EQDKP-PLUS %1$s o superior. Tienes instalado la versión %2$s",
+"sb_manage" => "Administrar",
+"sb_date_format" => "Y/m/d H:i",
+"sb_time_format" => "H:i",
+"sb_adm_date" => "Fecha",
+"sb_adm_name" => "Nombre",
+"sb_adm_text" => "Mensaje",
+"sb_adm_select_all" => "Seleccionar todos",
+"sb_adm_select_none" => "No seleccionar nada",
+"sb_config_saved" => "Los ajustes se han almacenado correctamente",
+"sb_header_general" => "Ajustes generales del Shoutbox",
+"sb_updatecheck" => "Habilitar casilla de verificación para las nuevas versiones Plugin",
+"sb_timezone" => "Zona horaria del servidor",
+"sb_dstcorrect" => "¿Ajustar automáticamente al horario de verano?",
+"sb_output_count_limit" => "Limitar entradas del shoutbox.",
+"sb_show_date" => "¿Mostrar las fechas?",
+"sb_show_archive" => "Mostrar Archivo?",
+"sb_input_box_below" => "¿Caja de texto debajo de las entradas?",
+"sb_autoreload" => "Cantidad de segundos a esperar para que recargue automaticamente el Shoutbox (Por defecto 0 = Off)",
+"sb_autoreload_help" => "Seleccionar 0 para desactivar la recarga automática",
+"sb_invisible_to_guests" => "<u>Invisible</u> para los invitados?",
+"sb_no_character_assigned" => "Aún no hay usuarios conectados. Al menos un usuario tiene que estar conectado para poder enviar una entrada.",
+"sb_submit_text" => "Envíar",
+"sb_save_wait" => "Almacenando, por favor espere...",
+"sb_reload" => "Recargar",
+"sb_no_entries" => "No hay entradas",
+"sb_archive" => "Archivo",
+"sb_shoutbox_archive" => "Archivo Shoutbox",
+"sb_footer" => "... %1$d encontrado / %2$d por páginas",
+"sb_about_header" => "Acerca de Shoutbox",
+"sb_credits_part1" => "Shoutbox v",
+"sb_credits_part2" => " por Aderyn",
+"sb_copyright" => "Copyright",
+ );
+$lang = (is_array($lang))? $lang : array();
+$lang = array_merge($lang, $alang);
 ?>
